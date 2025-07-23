b7b421c7b147282277ff48813944afa6600f0460
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2025-07-23 10:07:40 -0600

    feat: simple cubit

--- course_bloc/lib/main.dart
+++ course_bloc/lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:course_bloc/config/config.dart';
+import 'package:course_bloc/presentation/bloc/blocs.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const BlocProviders());
+}
+
+class BlocProviders extends StatelessWidget {
+  const BlocProviders({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => UsernameCubit())],
+      child: const MainApp(),
+    );
+  }
 }
 
 class MainApp extends StatelessWidget {
--- course_bloc/lib/presentation/screens/simple_cubit/cubit_screen.dart
+++ course_bloc/lib/presentation/screens/simple_cubit/cubit_screen.dart
@@ -1,13 +1,23 @@
+import 'package:course_bloc/config/helpers/random_generator.dart';
+import 'package:course_bloc/presentation/bloc/blocs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CubitScreen extends StatelessWidget {
   const CubitScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final usernameCubit = context.watch<UsernameCubit>();
     return Scaffold(
       appBar: AppBar(title: const Text('Cubit')),
-      body: const Center(child: Text('Repaso Bloc')),
+      body: Center(child: Text(usernameCubit.state)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          usernameCubit.setUsername(RandomGenerator.getRandomName());
+        },
+        child: const Icon(Icons.refresh),
+      ),
     );
   }
 }
